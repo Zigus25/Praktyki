6fbaa68e1e949c07c8926b143be974dc7ad18c74
Zigus25 <maciej.zet004@gmail.com>
2021-12-13 11:19:07 +0100

    Początek tworzenia aplikacji Excel

---
+++ Excel/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Forms;
+
+namespace Excel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CreateTable();
+        }
+        
+        public void CreateTable()
+        {
+            for(int i = 0; i < 26; i++)
+            {
+                DataGrid.Columns.Add(((char)(97+i)).ToString(), ((char)(65+i)).ToString());
+            }
+            for(int i = 0; i < 50; i++)
+            {
+                DataGrid.Rows.Add();
+            }
+        }
+    }
+}
