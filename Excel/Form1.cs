9cbbdf2d8189f7632da60b4e589d945fdc401405
Zigus25 <maciej.zet004@gmail.com>
2021-12-15 11:37:43 +0100

    Podstawowe operacje matematyczne

--- Excel/Form1.cs
+++ Excel/Form1.cs
@@ -71,21 +71,102 @@ namespace Excel
             int x = (DataGrid.CurrentCell.RowIndex + 1);
             string Kol = DataGrid.CurrentCell.OwningColumn.Name;
             Komurka.Text = (Kol + x);
+            bool tak = true;
             if (DataGrid.CurrentCell.Value!=null)
             {
                 string a = DataGrid.CurrentCell.Value.ToString();
                 label2.Text = a;
                 if (a.StartsWith("="))
                 {
-                    if (a.Contains("+"))
+                    if (a.Contains("*") && tak)
+                    {
+                        var sp = a.Remove(0, 1).Split("*");
+                        int wynik = 1;
+                        for (int i = 0; i < sp.Length; i++)
+                        {
+                            wynik *= Convert.ToInt32(sp[i]);
+                        }
+                        label2.Text = (wynik).ToString();
+                        tak = false;
+                    }
+                    if (a.Contains("/") && tak)
+                    {
+                        var sp = a.Remove(0, 1).Split("/");
+                        int wynik = 0;
+                        for (int i = 0; i < sp.Length; i++)
+                        {
+                            if (i==0)
+                            {
+                                wynik = Convert.ToInt32(sp[i]);
+                            }
+                            else
+                            {
+                                wynik /= Convert.ToInt32(sp[i]);
+                            }
+                        }
+                        label2.Text = (wynik).ToString();
+                        tak = false;
+                    }
+                    if (a.Contains("-") && tak)
+                    {
+                        var sp = a.Remove(0, 1).Split("-");
+                        int wynik = 0;
+                        for (int i = 0; i < sp.Length; i++)
+                        {
+                            if (sp[i].Contains("+"))
+                            {
+                                var susp = sp[i].Split("+");
+                                if (i != 0)
+                                {
+                                    wynik -= Convert.ToInt32(susp[0]);
+                                }
+                                for (int j = 1; j < susp.Length; j++)
+                                {
+                                    wynik += Convert.ToInt32(susp[j]);
+                                }
+                            }
+                            else
+                            {
+                                if (a.Remove(0, 1)[0] == '-' && i == 0)
+                                {
+                                    wynik -= Convert.ToInt32(sp[i+1]);
+                                    i++;
+                                }
+                                else if (i == 0)
+                                {
+                                    wynik += Convert.ToInt32(sp[i]);
+                                }
+                                else
+                                {
+                                    wynik -= Convert.ToInt32(sp[i]);
+                                }
+                            }
+                        }
+                        label2.Text = (wynik).ToString();
+                        tak = false;
+                    }
+                    if (a.Contains("+") && tak)
                     {
                         var sp = a.Remove(0,1).Split("+");
                         int wynik = 0;
                         for(int i = 0; i< sp.Length; i++)
                         {
-                            wynik += Convert.ToInt32(sp[i]);
+                            if (sp[i].Contains("-"))
+                            {
+                                var susp = sp[i].Split("-");
+                                wynik += Convert.ToInt32(susp[0]);
+                                for (int j =1; j< susp.Length; j++)
+                                {
+                                    wynik -= Convert.ToInt32(susp[j]);
+                                }
+                            }
+                            else
+                            {
+                                wynik += Convert.ToInt32(sp[i]);
+                            }
                         }
                         label2.Text = (wynik).ToString();
+                        tak = false;
                     }
                 }
             }
